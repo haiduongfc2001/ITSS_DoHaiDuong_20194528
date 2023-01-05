b10af9b6545c38bc7d976bebef3b45e868365785
haiduongfc2001 <haiduongfc2001@gmail.com>
2023-01-06 00:41:00 +0700

    Microservices With Express.js & MongoDB

--- books/books.js
+++ books/books.js
@@ -2,6 +2,14 @@
 const express = require("express");
 const app = express();
 
+// Load mongoose
+const mongoose = require("mongoose");
+
+// Connect
+mongoose.connect("mongodb+srv://haiduongfc2001:Haiduong26122001@cluster0.p383bze.mongodb.net/?retryWrites=true&w=majority", () => {
+    console.log("Connected to mongodb");
+});
+
 app.get("/", (req, res) => {
     res.send("This is the books services");
 })
